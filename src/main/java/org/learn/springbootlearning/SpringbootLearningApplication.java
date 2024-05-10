ea870324959fd0f9d31dd90e1356bba5f52f2320
Vishal <vg539616@gmail.com>
2024-05-10 21:26:29 +0530

    added starter

---
+++ src/main/java/org/learn/springbootlearning/SpringbootLearningApplication.java
@@ -0,0 +1,34 @@
+package org.learn.springbootlearning;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLearningApplication {
+
+	private static final Logger logger = LoggerFactory.getLogger(SpringbootLearningApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLearningApplication.class, args);
+		logger.info("Project started with gradle build tool..........");
+		// Create a Gson instance
+		final Gson gson = new Gson();
+
+		// Create a JsonObject and add dummy data
+		JsonObject jsonObject = new JsonObject();
+		jsonObject.addProperty("id", 1);
+		jsonObject.addProperty("name", "John Doe");
+		jsonObject.addProperty("email", "john.doe@example.com");
+
+		// Convert JsonObject to JSON string
+		String jsonString = gson.toJson(jsonObject);
+
+		// Print the JSON string
+		logger.info("JSON data - {}", jsonString);
+	}
+
+}
